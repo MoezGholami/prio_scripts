ba2a0331608fdb1c89541b8463e08af263e7846a
MoezGholami <Ali.Moez.Gholami@GMail.COM>
2016-10-22 18:15:24 -0500

    remove main method

--- src/main/java/edu/utexas/ece/moez/smallMaven/App.java
+++ src/main/java/edu/utexas/ece/moez/smallMaven/App.java
@@ -9,11 +9,6 @@ public class App
 
 	public static final String HELLO = "hello!";
 
-	public static void main(String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
-
     public static int addOf(int a, int b)
 	{
 		return a+b;
